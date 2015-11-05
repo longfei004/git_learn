20fc917679af4c7f0a10f630bd299c5983eec326
longfei004 <liulongfei004@yahoo.com>
2015-11-05 20:46:49 +0800

    get a serials of two numbers.

--- CPP_study/CPP_study/CPP_study_git.cpp
+++ CPP_study/CPP_study/CPP_study_git.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
-using namespace std;
+using namespace std;     //声明使用命名空间std中的名字
 
 int main()
 {
 	/****************变量区*****************/
 	int v1,v2 = 0;
+	int temp = 0;
 	/****************程序区*****************/
 	cout << "Enter Two Numbers:" << endl;
 	cin >> v1 >> v2;
-	cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
-
+	if(v1 > v2)
+	{ 
+		temp = v1;
+		v1 = v2;
+		v2 = temp;
+	}
+	temp = v1;
+    cout << "the resut is: " << endl;
+	while (temp <= v2)
+	{
+		cout << temp << endl;
+		++temp;
+	}
+	
 	return 0;
 }
