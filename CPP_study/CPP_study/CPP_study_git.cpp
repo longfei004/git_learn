001c745b4de4d801ecdc6a1e78d88c1d436bb938
longfei004 <liulongfei004@yahoo.com>
2015-11-05 19:41:29 +0800

    get numbers' sum.

--- CPP_study/CPP_study/CPP_study_git.cpp
+++ CPP_study/CPP_study/CPP_study_git.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
+	/****************变量区*****************/
+	int v1,v2 = 0;
+	/****************程序区*****************/
 	cout << "Enter Two Numbers:" << endl;
+	cin >> v1 >> v2;
+	cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
 
 	return 0;
 }
